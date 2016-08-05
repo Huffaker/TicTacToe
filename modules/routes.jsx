06b3e92df0de1421fa31591f8db138384e6fc382
James Huffaker <james.huffaker@gmail.com>
2016-08-04 17:46:59 -0700

    Currently, server doesn't support routing so removing

--- modules/routes.jsx
+++ modules/routes.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './App'
 import {HomeContainer} from './Home'
-import About from './About'
 
 module.exports = (
   <Route path="/" component={App}>
     <IndexRoute component={HomeContainer}/>
-    <Route path="/about" component={About}/>
   </Route>
 )
